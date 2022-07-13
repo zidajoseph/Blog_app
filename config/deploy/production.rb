cc0c90e3fc8117f0ea6943d44d7e7292f39eeef0
zidajoseph <zidajoseph60@gmail.com>
2022-07-13 12:23:30 +0000

    conf

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '44.207.123.204', user: 'ec2-user', roles: %w{app db web}
+server '44.207.123.204', user: 'ec2-user', roles: %w{ec2-user db web}
 set :ssh_options, keys: '/home/ec2-user/.ssh/id_rsa'
